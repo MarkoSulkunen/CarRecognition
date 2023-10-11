2f7c3f9db8d27d9df52c5dfb74376ed36a3c6ea2
Marko Sulkunen <sulkunen.marko@gmail.com>
2023-10-11 11:05:21 +0300

    rectangles

--- CarRecognition.py
+++ CarRecognition.py
@@ -1,20 +1,36 @@
 import cv2
 import streamlit as st
 import numpy as np
-from PIL import Image
-
+import io
 
 def main_loop():
     st.title("Car Recognition App")
-
+    
+    car_cascade = cv2.CascadeClassifier('cars.xml')
 
     image_file = st.file_uploader("Upload Your Image", type=['jpg', 'png', 'jpeg'])
     if not image_file:
         return None
 
-    car_image = Image.open(image_file)
-    car_image = np.array(car_image)
+    # Read the image file as bytes
+    image_bytes = image_file.read()
+    # Use io.BytesIO to convert the bytes-like object to a readable stream
+    image_stream = io.BytesIO(image_bytes)
+    # Read the image using OpenCV
+    car_image = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), -1)
+    
+    img_gray = cv2.cvtColor(car_image, cv2.COLOR_BGR2GRAY)
+    
+    found = car_cascade.detectMultiScale(img_gray, minSize=(20, 20))
 
+    # Draw rectangles around recognized cars and add text labels
+    for i, (x, y, width, height) in enumerate(found, 1):
+        cv2.rectangle(car_image, (x, y), (x + width, y + height), (0, 255, 0), 5)
+        text = f"Car {i}"
+        cv2.putText(car_image, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+
+    # Display the image using Streamlit
+    st.image(car_image, channels="BGR", use_column_width=True)
 
 if __name__ == '__main__':
     main_loop()
